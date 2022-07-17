e3f6a49fa197e36caf32e3fbd5c8edc8a4c5c04f
Adam Hurm <me@adamhurm.com>
2022-07-17 11:45:12 -0400

    super pal of the week - command and looped task

---
+++ README.md
@@ -0,0 +1,46 @@
+# super-pal-of-the-week
+Discord mod that promotes users to Super Pal of the Week role.
+
+This just runs in a tmux session on a raspberry pi.
+
+Currently this supports commands and the looped task.
+- **Command:**      Accepts `!spotw @user` commands to add another person to super pal of the week.
+- **Looped Task:**  Runs task on 7-day basis.
+
+--------
+
+## Setup
+
+Install dependencies:
+`pip install -U discord.py python-dotenv`
+
+
+Create a local file named `.env` and fill it with the following content:
+```
+DISCORD_TOKEN=
+GUILD_ID=
+CHANNEL_ID=
+```
+#### DISCORD_TOKEN
+- You can find or create a bot token in the [Discord developer portal](https://discord.com/developers/applications/).
+
+- Choose your application -> Go to Bot section -> Look under "Token" section \
+(This token can only be copied once so you may have to reset your token if you do not know it)
+
+#### GUILD_ID and CHANNEL_ID
+[Web Application](https://discord.com/app) (in-browser)
+
+- Click on the text channel in your server.
+
+- Look at your URL. It will be in the form of `https://discord.com/channels/[GUILD_ID]/[CHANNEL_ID]`
+
+
+
+[Desktop Application](https://discord.com/download)
+
+- Turn on `Developer Mode` in Settings -> Advanced
+
+- GUILD_ID: Right-click on your server icon and select `Copy ID`
+
+- CHANNEL_ID: Right click on the text channel where you want posts from super-pal-of-the-week-manager and select `Copy ID`
+
---
+++ discord-super-pal-of-the-week.py
@@ -0,0 +1,53 @@
+# discord-super-pal-of-the-week.py
+from http.client import ResponseNotReady
+import os
+import discord
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+from random import randrange
+
+# Load environmental variables.
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = os.getenv('GUILD_ID')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+# Required to list all users in a guild.
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Weekly Task: Choose "Super Pal of the Week"
+@tasks.loop(hours=24*7)
+async def super_pal_of_the_week():
+    guild = bot.get_guild(GUILD_ID)
+    channel = bot.get_channel(CHANNEL_ID)
+    role = discord.utils.get(guild.roles, name='super pal of the week')
+    # Get list of members and filter out bots.
+    true_member_list = [m for m in guild.members if not m.bot]
+    # Choose random "Super Pal of the Week" from list.
+    spotw = true_member_list[randrange(len(true_member_list))]
+
+    for member in true_member_list:
+        if role in member.roles and member == spotw:
+            print(f'{member.name} is already super pal of the week. Re-rolling.')
+            return await super_pal_of_the_week()
+        elif role in member.roles:
+            print(f'{member.name} has been removed from super pal of the week role.')
+            await member.remove_roles(role)
+        elif member == spotw:
+            await spotw.add_roles(role)
+            print(f'{member.name} has been added to super pal of the week role.')
+            await channel.send(f'Congratulations to {spotw.name}, the super pal of the week!')
+
+# Command: Promote Users to "Super Pal of the Week"
+@bot.command(name='spotw', pass_context=True)
+@commands.has_role('super pal of the week')
+async def add_super_pal(ctx, member: discord.Member):
+    role = discord.utils.get(ctx.guild.roles, name='super pal of the week')
+    if role not in member.roles:
+        await member.add_roles(role)
+
+super_pal_of_the_week.start()
+bot.run(TOKEN)
