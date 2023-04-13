fdc3dde6e4c6c9388e3384715f581030bb29c745
Adam Hurm <adamhurm@gmail.com>
2023-04-13 16:57:01 -0400

    remove tmux script from main branch

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-tmux new-session -d -s octoSession -n octoWindow
-tmux send-keys -t octoSession:octoWindow "cd /home/discord-super-pal-of-the-week && python3 super-pal.py " Enter
-tmux split-window -v
-tmux send-keys -t octoSession:octoWindow "export NVM_DIR=\"$HOME/.nvm\" && [ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"; cd /home/discord-super-pal-of-the-week/discord-spin-the-wheel && node dist/bot.js" Enter
-tmux attach -t octoSession:octoWindow
