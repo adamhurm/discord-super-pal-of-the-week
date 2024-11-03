b9973ec0a330024b8ccd0688353ff731401c6193
Adam Hurm <adamhurm@gmail.com>
2024-11-03 12:33:57 -0500

    remove VOICE_CHANNELS requirement

--- src/superpal/env.py
+++ src/superpal/env.py
@@ -22,20 +22,15 @@ GUILD_ID = int(os.environ['GUILD_ID'])
 EMOJI_GUILD_ID = GUILD_ID if os.environ['EMOJI_GUILD_ID'] is None else int(os.environ['EMOJI_GUILD_ID'])
 CHANNEL_ID = int(os.environ['CHANNEL_ID'])
 ART_CHANNEL_ID = CHANNEL_ID if os.environ['ART_CHANNEL_ID'] is None else int(os.environ['ART_CHANNEL_ID'])
-VOICE_CHANNELS = os.environ['VOICE_CHANNELS']
 GPT_ASSISTANT_ID = os.environ['GPT_ASSISTANT_ID']
 GPT_ASSISTANT_THREAD_ID = os.environ['GPT_ASSISTANT_THREAD_ID']
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
 base_reqnotmet = TOKEN is None or GUILD_ID is None or CHANNEL_ID is None
-karatechop_reqnotmet = VOICE_CHANNELS is None
 ai_reqnotmet = os.environ['OPENAI_API_KEY'] is None
 RUNTIME_WARN_MSG = 'WARN: Super Pal will still run but you are very likely to encounter run-time errors.'
 if base_reqnotmet:
     log.warn(f'Base requirements not fulfilled. Please provide TOKEN, GUILD_ID, CHANNEL_ID.\n'
-             f'{superpal_static.RUNTIME_WARN_MSG}\n')
-if karatechop_reqnotmet:
-    log.warn(f'Karate chop requirements not fulfilled. Please provide VOICE_CHANNELS.\n'
              f'{superpal_static.RUNTIME_WARN_MSG}\n')
 if ai_reqnotmet:
     log.warn(f'OpenAI requirements not fulfilled. Please provide api key.\n'
